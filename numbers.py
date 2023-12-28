ba84072807c4eb958c472d2c4fa1622fe47a8384
Isaac Goss <isaacgoss14750@gmail.com>
2023-12-28 10:35:55 -0500

    Generate Primes now returns full list, rather than iterator
    Oh, yeah, also, I've added a docstring of the function.

--- numbers.py
+++ numbers.py
@@ -1,30 +1,49 @@
 
 import math
 
+"""
+Generate a list of the first N prime numbers using a simple memoization scheme.
+
+Said list can be bounded by the number of primes to generate (nth_prime),
+or by the maximum value to check for primality (max_value).
+
+We simply iterate through the integers, and check if each is prime.
+In order to do so, we look for factors of the potential.
+That is, if we find a number such that their remainder after division is 0.
+
+Terms
+-----
+* The Potential: The number under question as to its primality.
+* Possible Factors: It is checked whether this number is in fact a factor of
+  the Potential in question. This is done using the modulous operator.
+
+In order to cut down on possible factors to check, we shall employ the following:
+    1. We already have the list of primes less than this potential.
+      We need only check if any of these evenly divide the potential, not all composites.
+      (If composite c=pq divides n, then so shall p and q)
+    2. It is only necessary to check up to the square root of the potential.
+      Any factors greater than the square root will have a corresponding co-factor
+      less than the square root.
+
+Leaving us with a rough time complexity to find the nth prime of O(n*sqrt(n))
+Tradeoff is that we use O(n) space along the way.
+"""
 def generate_primes(bound):
     primes = [2] # Begin with the first prime
-    yield 2
     for potential in range(2, bound):
+        # Relatively quick floating-point calculation for later comparison.
         sq_potential = math.sqrt(potential)
-        #print("Assessing potential {}, sqrt is {}".format(potential, sq_potential))
-        #print("Primes: {}".format(primes))
         # Try all primes up until the square root
         # of potential. If none of them evenly divide
         # potential, then it is in fact a prime
         for prime in primes:
             # Not a prime; move on
             if potential % prime == 0:
-                #print("  Prime {} divides potential {}".format(prime, potential))
                 break
             # Got past sqrt(potential)! It is a prime!
             if prime > sq_potential:
-                #print("  Because prime {} > sqrt {}: Potential {} is prime!".format(
-                    #prime,
-                    #sq_potential,
-                    #potential
-                #))
                 primes.append(potential)
-                yield potential
                 break
+    return primes
 
 
